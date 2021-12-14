117b97b404b9cf12674fb53655d2b1d7d9c8f8e9
bebestcode <bebestcode@gmail.com>
2021-12-14 18:11:29 +0900

    AllowHosts 수정.

--- app/settings.py
+++ app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['covid.univmind.net', '192.168.0.18']
 
 
 # Application definition
